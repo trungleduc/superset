bd74a461e164cc913756e20ed2210a5bf7f6dd26
Duc Trung Le <leductrungxf@gmail.com>
2025-12-25 19:35:00 +0100

    add bootstrap data

--- superset-frontend/src/sqlapp/index.tsx
+++ superset-frontend/src/sqlapp/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom';
 import { App } from './App';
-9/0
 
 const el = document.getElementById('app');
-console.log('heeofsssssssssssssre', el);
+
 ReactDOM.render(<App />, el);
--- superset-jupyter/scripts/build.js
+++ superset-jupyter/scripts/build.js
@@ -4,11 +4,16 @@ const path = require("path");
 const manifestPath = path.join(__dirname, "../static/assets/manifest.json");
 const templatePath = path.join(__dirname, "../index.template.html");
 const outputPath = path.join(__dirname, "../index.html");
-
+const bootstrapPath = path.join(__dirname, "./bootstrap.json");
 // Read files
 const manifest = JSON.parse(fs.readFileSync(manifestPath, "utf-8"));
 const htmlTemplate = fs.readFileSync(templatePath, "utf-8");
-
+const bootstrapData  = JSON.parse(fs.readFileSync(bootstrapPath, "utf-8"));
+const bootstrapString = JSON.stringify(bootstrapData)
+  .replace(/&/g, "&amp;")
+  .replace(/"/g, "&quot;")
+  .replace(/</g, "&lt;")
+  .replace(/>/g, "&gt;");
 // Collect JS assets (deduplicated)
 const jsAssets = new Set();
 
@@ -20,11 +25,13 @@ for (const entrypoint of Object.values(manifest.entrypoints)) {
 
 // Generate <script> tags
 const scriptTags = Array.from(jsAssets)
-  .map(src => `       <script src=".${src}"></script>`)
+  .map((src) => `       <script src=".${src}"></script>`)
   .join("\n");
 
 // Replace placeholder
-const finalHtml = htmlTemplate.replace("{{JS_ASSETS}}", scriptTags);
+const finalHtml = htmlTemplate
+  .replace("{{JS_ASSETS}}", scriptTags)
+  .replace("{{BOOTSTRAP}}", bootstrapString );
 
 // Write output
 fs.writeFileSync(outputPath, finalHtml);
